e9c0362743aac6aa448c6805353b8dda7c800505
ECRSSS <n.n.glebanov@gmail.com>
2020-02-16 14:15:40 +0300

    finish frontend

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
       title: 'SPRING LIBRARY',
       act: 0,
       index: '',
-      books: [],
+      books: [{authors:[]}],
       isButtonsDisabled: false
     }
   }
@@ -27,9 +27,7 @@ class App extends React.Component {
           console.log(result);
 
         },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
+
         (error) => {
           this.setState({
             isLoaded: true,
@@ -47,18 +45,18 @@ class App extends React.Component {
 
     let datas = this.state.books;
     let bookTitle = this.refs.bookTitle.value;
-    //let authors = []
-    //let author = new Object()
-    //author.firstName = this.refs.authorName.value;
-    //author.lastName = this.refs.authorLastName.value;
-    //authors.push(author)
+    let authors = []
+    let author = new Object()
+    author.firstName = this.refs.authorName.value;
+    author.lastName = this.refs.authorLastName.value;
+    authors.push(author)
 
 
 
     if (this.state.act === 0) { //new
       let data = {
         bookTitle,
-        //authors
+        authors
       }
       datas.push(data);
       var request = new XMLHttpRequest();
@@ -68,7 +66,7 @@ class App extends React.Component {
     } else {                    //update
       let index = this.state.index;
       datas[index].bookTitle = bookTitle;
-      //////datas[index].authors = authors
+      datas[index].authors = authors
       var request = new XMLHttpRequest();
       request.open('PUT', 'http://localhost:8090/api/books/'+(datas[index].id));
       request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
@@ -104,8 +102,8 @@ class App extends React.Component {
   fEdit = (i) => {
     let book = this.state.books[i];
     this.refs.bookTitle.value = book.bookTitle;
-    //this.refs.authorName.value = book.authors[0].firstName;
-    //this.refs.authorLastName.value = book.authors[0].lastName;
+    this.refs.authorName.value = book.authors[0].firstName;
+    this.refs.authorLastName.value = book.authors[0].lastName;
 
 
     this.setState({
@@ -119,19 +117,20 @@ class App extends React.Component {
 
   render() {
     let books = this.state.books;
+    console.log(books);
     return (
       <div className="App">
         <h2>{this.state.title}</h2>
         <form ref="myForm" className="myForm">
           <input type="text" ref="bookTitle" placeholder="Book title" className="formField" />
-          {/*<input type="text" ref="authorName" placeholder="Author first name" className="formField" />*/}
-          {/*<input type="text" ref="authorLastName" placeholder="Author last name" className="formField" />*/}
+          <input type="text" ref="authorName" placeholder="Author first name" className="formField" />
+          <input type="text" ref="authorLastName" placeholder="Author last name" className="formField" />
           <button onClick={(e) => this.fSubmit(e)} className="myButton">Submit</button>
         </form>
         <pre>
           {books.map((book, i) =>
             <li key={i} className="myList">
-              {book.id}.{book.bookTitle}{/*, {book.authors.map(author => author.firstName + " " + author.lastName).join(",")}*/}
+              {i+1}.{book.bookTitle}, {book.authors.map(author => author.firstName + " " + author.lastName).join(",")}
               <button onClick={() => this.fRemove(i)} className="myListButton" disabled={this.state.isButtonsDisabled}>Remove</button>
               <button onClick={() => this.fEdit(i)} className="myListButton" disabled={this.state.isButtonsDisabled}>Edit</button>
             </li>
